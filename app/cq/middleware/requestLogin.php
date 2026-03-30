617598c89ea5457fe23fcc510697147c53188d1e
wufan <826518711@qq.com>
2026-03-30 22:38:51 +0800

    过滤monster_list请求

--- app/cq/middleware/requestLogin.php
+++ app/cq/middleware/requestLogin.php
@@ -14,7 +14,8 @@ class requestLogin
     protected $excludeUris = [
         'user/getWechatInfo',
         'user/setCoinShare',
-        'user/get_user_info_h52'
+        'user/get_user_info_h52',
+        'kill/monster_list'
     ];
     public function handle(Request $request, \Closure $next){
         // 设置 CORS 头部（通常在全局中间件或响应中设置更合适）
