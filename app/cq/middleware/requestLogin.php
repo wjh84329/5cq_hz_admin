8136ce921bc2517642d023c1b4893d2995d41f62
wufan <826518711@qq.com>
2026-03-30 23:18:54 +0800

    2秒重复再加过滤

--- app/cq/middleware/requestLogin.php
+++ app/cq/middleware/requestLogin.php
@@ -15,7 +15,8 @@ class requestLogin
         'user/getWechatInfo',
         'user/setCoinShare',
         'user/get_user_info_h52',
-        'kill/monster_list'
+        'kill/monster_list',
+        'goods/order_list'
     ];
     public function handle(Request $request, \Closure $next){
         // 设置 CORS 头部（通常在全局中间件或响应中设置更合适）
